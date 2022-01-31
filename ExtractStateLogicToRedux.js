e147e2e7b55d7bbea4d5bcfa39fc95abe6a697ec
sankyk2020 <69984129+sanky2020@users.noreply.github.com>
2022-02-01 01:53:44 +0530

    Update ExtractStateLogicToRedux.js

--- ExtractStateLogicToRedux.js
+++ ExtractStateLogicToRedux.js
@@ -24,3 +24,66 @@ const messageReducer =(state=[],action)=>{
 
 //Redux store 
 const store = Redux.createStore(messageReducer)
+
+
+
+// React:
+
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  submitMessage() {  
+    this.setState((state) => {
+      const currentMessage = state.input;
+      return {
+        input: '',
+        messages: state.messages.concat(currentMessage)
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input
+          value={this.state.input}
+          onChange={this.handleChange}/><br/>
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.state.messages.map( (message, idx) => {
+              return (
+                 <li key={idx}>{message}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+};
+
+const Provider = ReactRedux.Provider;
+
+class AppWrapper extends React.Component {
+    
+    render() {
+      return (
+        <Provider store={store}>
+          <DisplayMessages />
+        </Provider>
+      );
+    }
+    
+  };
